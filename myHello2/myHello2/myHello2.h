14eafb00644a5a9c23042beb4e373fd55a763cd8
Tom Xue <tomxue0126@gmail.com>
2014-08-10 19:39:20 +0800

    add C dll experimental code, which can be invoked in python

---
+++ myHello2/myHello2/myHello2.h
@@ -0,0 +1,28 @@
+// myHello2.h : main header file for the myHello2 DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CmyHello2App
+// See myHello2.cpp for the implementation of this class
+//
+
+class CmyHello2App : public CWinApp
+{
+public:
+	CmyHello2App();
+	void sayHello();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
